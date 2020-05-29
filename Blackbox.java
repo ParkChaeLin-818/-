8389201db7d596833f23062158656977ed2605f3
iwamegu1208 <iwamegu1208@yahoo.com>
2020-05-29 11:34:02 +0900

    //문

--- Blackbox.java
+++ Blackbox.java
@@ -9,20 +9,26 @@ public class Blackbox
 {
     public static void main(String[] args){
         int x = 0;
+        //16세이상인 경우
         if (150 > x && x > 16){
             System.out.print("2000원 입니다.");
         }
+        //7세이상인 경우
         else if(x > 7){
             System.out.print("900원 입니다.");
         }
+        //4세이상인 경우
         else if(x > 4){
             System.out.print("400원 입니다.");
         }
+        //4세미만인 경우
         else if(x > 0){
             System.out.print("0원 입니다.");
         }
+        //잘못된 경우
         else{
             System.out.print("잘못된 값 입니다.");
         }
         
-    }}
+    }
+}
