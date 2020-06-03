6ade6af454070ff747970d96bf0ab80378dba2a3
keet <wwukuww@gmail.com>
2020-06-03 12:05:09 +0900

    finalvarion

--- Blackbox.java
+++ Blackbox.java
@@ -1,33 +1,28 @@
 
 /**
- * 여기에 Blackbox 클래스 설명을 작성하십시오.
+ * ABC수족관 입장료를 계산 하는 메소드
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
 public class Blackbox
 {
-    public static void main(String[] args){
-        int x = 0;
-        //16세이상인 경우
-        if (150 > x && x > 16){
-            System.out.print("2000원 입니다.");
+    int age = 0;
+    public  int caluculate(int age){
+        if ( age > 16){
+            return 2000;
         }
-        //7세이상인 경우
-        else if(x > 7){
-            System.out.print("900원 입니다.");
+        else if(age > 7){
+            return 900;
         }
-        //4세이상인 경우
-        else if(x > 4){
-            System.out.print("400원 입니다.");
+        else if(age >= 4){
+            return 400;
         }
-        //4세미만인 경우
-        else if(x > 0){
-            System.out.print("0원 입니다.");
+        else if(age >= 0){
+            return 0;
         }
-        //잘못된 경우
         else{
-            System.out.print("잘못된 값 입니다.");
+            return 99999;
         }
         
     }
