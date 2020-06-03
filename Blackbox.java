f522d24d14f1422f663fa11fe7f96a22384ccaa7
iwamegu1208 <iwamegu1208@yahoo.co.jp>
2020-06-03 12:22:17 +0900

    error

--- Blackbox.java
+++ Blackbox.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 /**
  * 여기에 Blackbox 클래스 설명을 작성하십시오.
@@ -32,3 +33,39 @@ public class Blackbox
         
     }
 }
+=======
+
+/**
+ * ABC수족관 입장료를 계산하는 메소드
+ * 
+ * @author (박채린,마츠바라 케이토,이와사키 메구미) 
+ * @version (20200603)
+ */
+public class Blackbox
+{
+    /**
+     * calculate = 수족관 입장료를 계산하는 메소드
+     * @param = age
+     * return 타입은 int 입니다.
+     */
+    int age = 0;
+    public  int calculate(int age){
+        if ( age > 16){
+            return 2000;
+        }
+        else if(age > 7){
+            return 900;
+        }
+        else if(age >= 4){
+            return 400;
+        }
+        else if(age >= 0){
+            return 0;
+        }
+        else{
+            return 99999;
+        }
+        
+    }
+}
+>>>>>>> 045f424c8eb95bdc03c95cf6685af551c92f3a83
